46716b129055ef604c8cc37960b085d7c0cdf8c7
Alehandro Persaud <andrew041994@gmail.com>
2020-02-05 17:32:54 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,7 +8,7 @@ attr_accessor :path
     @filename = []
     file = Dir['**/*.mp3']
     file.each do |filename|
-      @filename << File.basename("#{filename}")
+        @filename << File.basename("#{filename}")
       end
       @filename
   end
@@ -16,6 +16,4 @@ attr_accessor :path
   def import
     files.each {|filename|Song.new_by_filename(filename)}
   end
-
-
 end
