cdaf15eabf02a559c7e465e9f89dd25c71ac2026
Alehandro Persaud <andrew041994@gmail.com>
2020-02-05 17:10:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,9 @@ class Song
     name = name.split("-")
     name = name.collect(&:strip)
     new_song = Song.new(name[1])
-    new_song
+    new_song.artist= Artist.find_or_create_by_name(name[0])
+    new_song.artist.add_song(new_song)
+    # new_song
     # binding.pry
 
   end
